e8615ed036417a767dfba3b79c7adb626be85316
Angelo <macaldo.jonangelo@gmail.com>
2026-03-01 12:08:40 +0800

    remove old tools section

--- src/app/page.js
+++ src/app/page.js
@@ -126,60 +126,6 @@ export default function MyPortfolio() {
               </div>
             </div>
           </div>
-
-          {/* Tools Column */}
-          <div>
-            <h2 className="text-2xl md:text-3xl lg:text-3xl font-bold opacity-90 mb-6 md:mb-8">
-              Tools
-            </h2>
-            <div className="grid grid-cols-4 sm:grid-cols-4 gap-6 md:gap-8 max-w-xs sm:max-w-sm">
-              <div className="flex flex-col items-center">
-                <FaReact className="text-black hover:text-blue-500 text-4xl md:text-5xl lg:text-6xl transition-colors duration-300" />
-                <p className="mt-2 text-xs md:text-sm font-medium opacity-80">
-                  React
-                </p>
-              </div>
-              <div className="flex flex-col items-center">
-                <SiTailwindcss className="text-black hover:text-blue-400 text-4xl md:text-5xl lg:text-6xl transition-colors duration-300" />
-                <p className="mt-2 text-xs md:text-sm font-medium opacity-80">
-                  TailwindCSS
-                </p>
-              </div>
-              <div className="flex flex-col items-center">
-                <FaCss3 className="text-black hover:text-blue-700 text-4xl md:text-5xl lg:text-6xl transition-colors duration-300" />
-                <p className="mt-2 text-xs md:text-sm font-medium opacity-80">
-                  CSS
-                </p>
-              </div>
-              <div className="flex flex-col items-center">
-                <FaHtml5 className="text-black hover:text-orange-500 text-4xl md:text-5xl lg:text-6xl transition-colors duration-300" />
-                <p className="mt-2 text-xs md:text-sm font-medium opacity-80">
-                  HTML
-                </p>
-              </div>
-              <div className="flex flex-col items-center">
-                <FaJsSquare className="text-black hover:text-yellow-500 text-4xl md:text-5xl lg:text-6xl transition-colors duration-300" />
-                <p className="mt-2 text-xs md:text-sm font-medium opacity-80">
-                  JavaScript
-                </p>
-              </div>
-              <div className="flex flex-col items-center">
-                <RiNextjsFill
-                  className="text-black hover:text-black text-4xl md:text-5xl
-                lg:text-6xl transition-colors duration-300"
-                />
-                <p className="mt-2 text-xs md:text-sm font-medium opacity-80">
-                  NextJS
-                </p>
-              </div>
-              <div className="flex flex-col items-center">
-                <FaFigma className="text-black hover:text-violet-600 text-4xl md:text-5xl lg:text-6xl transition-colors duration-300" />
-                <p className="mt-2 text-xs md:text-sm font-medium opacity-80">
-                  Figma
-                </p>
-              </div>
-            </div>
-          </div>
         </div>
       </section>
 
